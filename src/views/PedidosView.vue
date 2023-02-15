e6527cafb921b8b52a17128b40b04885b29d7444
juliocesar014 <juliocesar014@live.com>
2023-02-15 11:08:05 -0300

    feat: Create html/css dashboard

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <DashboardComponente />
   </div>
 </template>
+
+<script>
+import DashboardComponente from "@/components/DashboardComponente.vue";
+
+export default {
+  name: "PedidosView",
+  components: {
+    DashboardComponente,
+  },
+};
+</script>
+
+<style scoped></style>
